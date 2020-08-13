1a6a4782fb52accef16246d3fed2d3200ab81f4f
Gustavo Compiano <gucompi@gmail.com>
2020-08-13 14:43:14 -0300

    .

--- desafio1.js
+++ desafio1.js
@@ -174,7 +174,7 @@ fecha	cierre
 1) Crear un algoritmo que detecte: 
     a) Valor minimo
     b) Valor maximo
-    c) Valor promedio
+    c) Valor promedio.
 
 2) Estos Algoritmos deben poder funcionar con cualquier set de cotizaciones de cualquier empresa.
 
